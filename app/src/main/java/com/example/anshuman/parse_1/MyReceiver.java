67eb781d1be5cb3a7f014c08bf938334540f55b1
anshumanthakur <anshumanth1997@gmail.com>
2018-01-13 12:59:59 +0530

    commit_3

--- app/src/main/java/com/example/anshuman/parse_1/MyReceiver.java
+++ app/src/main/java/com/example/anshuman/parse_1/MyReceiver.java
@@ -3,18 +3,79 @@ package com.example.anshuman.parse_1;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.TelephonyManager;
 import android.widget.Toast;
 
+import java.util.Date;
+
 public class MyReceiver extends BroadcastReceiver {
+    private static Date callStartTime;
+    private static boolean isIncoming;
+    private static String savedNumber;
+    private static int lastState = TelephonyManager.CALL_STATE_IDLE;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
         String action = intent.getAction();
-        if(action.equals(Intent.ACTION_NEW_OUTGOING_CALL)){
-            Toast.makeText(context, "hey..", Toast.LENGTH_SHORT).show();
+        if (intent.getAction().equals("android.intent.action.NEW_OUTGOING_CALL")) {
+            savedNumber = intent.getExtras().getString("android.intent.extra.PHONE_NUMBER");
+
+        } else {
+            String stateStr = intent.getExtras().getString(TelephonyManager.EXTRA_STATE);
+            String number = intent.getExtras().getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+            int state = 0;
+            if (stateStr.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+                state = TelephonyManager.CALL_STATE_IDLE;
+            } else if (stateStr.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
+                state = TelephonyManager.CALL_STATE_OFFHOOK;
+            } else if (stateStr.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                state = TelephonyManager.CALL_STATE_RINGING;
+            }
+            onCallStateChanged(context, state, number);
         }
-        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    public void onCallStateChanged(Context context, int state, String number) {
+        if(lastState == state){
+            //No change, debounce extras
+            return;
+        }
+        switch (state) {
+            case TelephonyManager.CALL_STATE_RINGING:
+                isIncoming = true;
+                callStartTime = new Date();
+                savedNumber = number;
+
+                Toast.makeText(context, "Incoming Call Ringing" , Toast.LENGTH_SHORT).show();
+                break;
+            case TelephonyManager.CALL_STATE_OFFHOOK:
+                //Transition of ringing->offhook are pickups of incoming calls.  Nothing done on them
+                if(lastState != TelephonyManager.CALL_STATE_RINGING){
+                    isIncoming = false;
+                    callStartTime = new Date();
+                    Toast.makeText(context, "Outgoing Call Started" , Toast.LENGTH_SHORT).show();
+                }
+
+                break;
+            case TelephonyManager.CALL_STATE_IDLE:
+                //Went to idle-  this is the end of a call.  What type depends on previous state(s)
+                if(lastState == TelephonyManager.CALL_STATE_RINGING){
+                    //Ring but no pickup-  a miss
+                    Toast.makeText(context, "Ringing but no pickup" + savedNumber + " Call time " + callStartTime +" Date " + new Date() , Toast.LENGTH_SHORT).show();
+                }
+                else if(isIncoming){
+
+                    Toast.makeText(context, "Incoming " + savedNumber + " Call time " + callStartTime  , Toast.LENGTH_SHORT).show();
+                }
+                else{
+
+                    Toast.makeText(context, "outgoing " + savedNumber + " Call time " + callStartTime +" Date " + new Date() , Toast.LENGTH_SHORT).show();
+
+                }
+
+                break;
+        }
+        lastState = state;
     }
 }
